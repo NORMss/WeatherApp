44881fd368b0e47c544118087695a431558f8715
NORMss <serega.b301@gmail.com>
2023-12-19 10:21:02 +0700

    feature(Icons): feature Icons in MainScreen
    Closes #2

--- app/src/main/java/com/norm/myappweather/screens/MainScreen.kt
+++ app/src/main/java/com/norm/myappweather/screens/MainScreen.kt
@@ -1,7 +1,6 @@
 package com.norm.myappweather.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,6 +11,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +27,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.norm.myappweather.R
-import com.norm.myappweather.ui.theme.LightGrey
 
 @Preview(showBackground = true)
 @Composable
@@ -51,11 +50,11 @@ fun MainScreen() {
             elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
             shape = RoundedCornerShape(10.dp)
         ) {
-            Column (
+            Column(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                Row (
+            ) {
+                Row(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
@@ -64,13 +63,59 @@ fun MainScreen() {
                         text = "18 Dec 2023 16:54",
                         style = TextStyle(fontSize = 15.sp),
                         color = Color.White
-                        )
+                    )
                     AsyncImage(
                         model = "https://cdn.weatherapi.com/weather/128x128/night/113.png",
                         contentDescription = "im2",
-                        modifier = Modifier.size(40.dp)
+                        modifier = Modifier
+                            .size(40.dp)
                             .padding(top = 3.dp, end = 8.dp)
+                    )
+                }
+                Text(
+                    text = "Novosibirsk",
+                    style = TextStyle(fontSize = 24.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "-20°C",
+                    style = TextStyle(fontSize = 65.sp),
+                    color = Color.White
+                )
+                Text(
+                    text = "Clear",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.White
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(onClick = {
+
+                    }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "im3",
+                            tint = Color.White
                         )
+                    }
+                    Text(
+                        text = "-16°C/-19°C",
+                        style = TextStyle(fontSize = 16.sp),
+                        color = Color.White
+                    )
+                    IconButton(onClick = {
+
+                    }
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_sync),
+                            contentDescription = "im4",
+                            tint = Color.White
+                        )
+                    }
                 }
             }
         }
